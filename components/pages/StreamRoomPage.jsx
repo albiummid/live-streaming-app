5a0f7126ac3ac7daeb7d976c611db33239285a72
albiummid <albi.ummid@gmail.com>
2023-04-16 13:07:01 +0600

    single streaming done with chat functionality

--- components/pages/StreamRoomPage.jsx
+++ components/pages/StreamRoomPage.jsx
@@ -5,22 +5,20 @@ import { peer } from '@/utils/PeerInstance';
 import socket from '@/utils/Socket';
 import { Affix, TextInput } from '@mantine/core';
 import { useRouter } from 'next/router';
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import { toast } from 'react-hot-toast';
+import { AiOutlineSend } from 'react-icons/ai';
 import VideoPlayer from '../ui/VideoPlayer';
 
 export default function StreamRoom() {
     const router = useRouter();
-
     const {roomId} = router.query
     const {data:roomData} = useGetStreamQuery(roomId,{
         skip:!roomId
     });
     const {user} = useAuthState();
-
     const {getLocalMediaStream,localMediaStream} = useWebRTC();
     const isHost = useMemo(()=>user?._id === roomData?.createdBy,[user,roomData]);
-
     const streamConfigs = ()=>{
       if(isHost){
         return {
@@ -32,7 +30,6 @@ export default function StreamRoom() {
         video:true}
       }
     }
-
     const [peerId, setPeerId] = useState("");
     const [joined,setJoined] = useState(false);
     const [remoteStreams, setRemoteStreams] = useState([]);
@@ -41,69 +38,73 @@ export default function StreamRoom() {
     const [localUserStream, setLocalUserStream] = useState(null);
     const [callList,setCallList] = useState([])
     const [answerList,setAnswerList] = useState([])
+    const [messageField,setMessageField] = useState('');
+    const [messages,setMessages] = useState([]);
+    const bottomRef = useRef(null);
+
 
     
 
  
   
     useEffect(() => {
+     if(isHost){
       getLocalMediaStream();
-    }, []);
+     }
+    }, [isHost]);
 
     useEffect(()=>{
-      peer.once("open", (id) => {
-        setPeerId(id);
-        toast.success(`You are connected with server !`)
-        if(roomId && user){
-          socket.emit("join-room", {
-            roomId,
-            userId: user?._id,
-            peerId:id,
-          });
-        }
-      });
-
-      peer.on("call", async (call) => {
-        try {
-          // Checking , is the call already received ?
-          if (remoteStreams.find((x) => x.peerId == call.peer)) {
-            return toast.error("already answered");
-          }
-
-          global.navigator.mediaDevices
-          .getUserMedia(streamConfigs())
-          .then((mediaStream) => {
-            call.answer(mediaStream);
-            // Sending my stream to remoteUser
-            call.on("stream", function (remoteStream) {
-              let remotePeerId = call.peer;
-              setRemoteStreams((prv) =>
-                prv
-                  .filter((x) => x.peerId !== call.peer)
-                  .concat({
-                    peerId: remotePeerId,
-                    stream: remoteStream,
-                  })
-              );
+      function initPeer (){
+        peer.once("open", (id) => {
+          setPeerId(id);
+          toast.success(`You are connected with server !`)
+          if(roomId && user){
+            socket.emit("join-room", {
+              roomId,
+              userId: user?._id,
+              peerId:id,
             });
-          })
-        
+          }
+        });
+  
+        peer.on("call", async (call) => {
+          try {
+            // Checking , is the call already received ?
+            if (remoteStreams.find((x) => x.peerId == call.peer)) {
+              return toast.error("already answered");
+            }
+            global.navigator.mediaDevices
+            .getUserMedia(streamConfigs())
+            .then((mediaStream) => {
+              call.answer(mediaStream);
+              // Sending my stream to remoteUser
+              call.on("stream", function (remoteStream) {
+                let remotePeerId = call.peer;
+                setRemoteStreams((prv) =>
+                  prv
+                    .filter((x) => x.peerId !== call.peer)
+                    .concat({
+                      peerId: remotePeerId,
+                      stream: remoteStream,
+                    })
+                );
+              });
+            })
 
-          call.on('close',()=>{
-            console.log(`peerID:${call.peer} is disconnected !`)
-            let streams = remoteStreams;
-            setRemoteStreams([]);
-            setRemoteStreams(prv=>streams.filter(x=>x.peerId !== call.peer));
-          })
-
-        } catch (err) {
-          console.log(err, "error..");
-        }
-      });
-        return ()=>{
-          peer.off('open')
-          peer.off('call')
-        }
+  
+            call.on('close',()=>{
+              console.log(`peerID:${call.peer} is disconnected !`)
+              let streams = remoteStreams;
+              setRemoteStreams([]);
+              setRemoteStreams(prv=>streams.filter(x=>x.peerId !== call.peer));
+            })
+  
+          } catch (err) {
+            console.log(err, "error..");
+          }
+        });
+      }
+        return initPeer()
     },[roomId,user,localMediaStream])
 
     async function call({ remotePeerId, remoteSocketId, user }) {
@@ -151,31 +152,74 @@ export default function StreamRoom() {
         }
       }
     
+const messageScrollToBottom = (id)=>{
+  setTimeout(()=>{
+    let el = document.getElementById(id)
+    el?.scrollIntoView({behavior: 'smooth', top:100})
+  },10)
+}
+
 
     useEffect(() => {
-     
-      return () => {
-        socket.on(`user-joined-at-${roomId}`, (payload) => {
-          toast.success("A user joined !");
-          if (payload.data.success) {
-            if (payload.data.peerId != peerId) {
-              call({
-                remotePeerId: payload.data.peerId,
-                remoteSocketId: payload.data.socketId,
-                user: payload.data.user,
-              });
-            }
-          } else {
-            console.log(payload, "payload error... >");
+      socket.on(`user-joined-at-${roomId}`, (payload) => {
+        toast.success("A user joined !");
+        if (payload.data.success) {
+          if (payload.data.peerId != peerId) {
+            call({
+              remotePeerId: payload.data.peerId,
+              remoteSocketId: payload.data.socketId,
+              user: payload.data.user,
+            });
           }
-        });
-    
-        socket.on(`user-disconnected-from-${roomId}`, (payload) => {
-          // setRemoteStreams(prv=>prv.filter(x=>x.socketId !== payload.data.socketId))
-          setRemoteStreams([]);
-        });
+        } else {
+          console.log(payload, "payload error... >");
+        }
+      });
+  
+      socket.on(`user-disconnected-from-${roomId}`, (payload) => {
+        setRemoteStreams([]);
+      });
+
+      socket.on(`streamRoom-${roomId}/message`,(payload)=>{
+        setMessages(prv=>prv.concat(payload.data.message))
+        console.log(payload.data.message.sentBy?._id)
+        if(payload.data.message.sentBy?._id === user?._id){
+          setMessageField("")
+        }
+
+        messageScrollToBottom(payload.data.message._id);
+      })
+      return () => {
+        socket.off(`streamRoom-${roomId}/message`)
+        socket.off(`user-disconnected-from-${roomId}`)
+        socket.off(`user-joined-at-${roomId}`)
       };
-    }, [roomId, peerId]);
+    }, [roomId, peerId,user]);
+
+    const handleSendMessage = ()=>{
+      if(!messageField.length) return
+      socket.emit('streamRoom/message',{
+        roomId,
+        userId:user?._id,
+        body:messageField,
+        kind:'Text'
+      })
+    }
+
+    const MessageContainer = ({body,kind,sentBy,_id})=>{
+      const ownMessage = sentBy._id === user._id;
+      if(kind === 'Text'){
+        return <div id={_id} className={`my-2  w-fit ${ownMessage?'ml-auto':''}`}>
+         {
+          ownMessage? <small className=' text-right '>
+        </small>: <small className='text-[10px] pl-1'>
+            {sentBy.name}
+          </small>
+         }
+          <p className={`border rounded-lg w-fit p-2 text-xs ${ownMessage ? 'bg-slate-400/20':'bg-red-400/20'}`}>{body}</p>
+        </div>
+      }
+    }
 
     
   return (
@@ -199,7 +243,16 @@ export default function StreamRoom() {
       </section>
 
       <Affix className='w-full' bottom={0}>
-        <TextInput className=" w-full"/>
+        <div className='w-full max-h-[20vh] overflow-y-scroll px-5'> 
+          {
+            messages?.map(x=><MessageContainer {...x} key={x._id}/>)
+          }
+        </div>
+        <TextInput   onKeyPress={(e) => {
+    if (e.key === 'Enter') {
+      handleSendMessage()
+    }
+  }} value={messageField} onChange={(e)=>setMessageField(e.currentTarget.value)} className=" w-full" rightSection={<AiOutlineSend className=' cursor-pointer' onClick={handleSendMessage}/>}/>
       </Affix>
     </main>
   );
